43017f68eb559a0ff80cee59d90c9f129bcbaad6
Alexandre Cruz <alexandre.machcruz@gmail.com>
2025-01-27 15:44:57 -0300

    apply code review suggestions

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -35,9 +35,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
-  ]
+  plugins: []
 }
